41110d4c083b1708709e9d385a355f49db954a2b
Soyoung Oh <sori424@gmail.com>
2022-10-20 22:27:07 +0200

    revise imple

--- implementations.py
+++ implementations.py
@@ -1,50 +1,14 @@
 import numpy as np
 
 from scripts.proj1_helpers import batch_iter, predict_binary
-from scripts.regression.loss import log_likelihood_loss
-from scripts.regression.gradient import gradient_log_likelihood
+from scripts.regression.loss import compute_loss, log_likelihood_loss
+from scripts.regression.gradient import compute_gradient, gradient_log_likelihood
 
 
 
 """
 Gradient Descent
 """
-import numpy as np
-
-def compute_loss(y, tx, w):
-    """Calculate the loss using either MSE or MAE.
-    Args:
-        y: shape=(N, )
-        tx: shape=(N,2)
-        w: shape=(2,). The vector of model parameters.
-    Returns:
-        the value of the loss (a scalar), corresponding to the input parameters w.
-    """
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: compute loss by MSE
-    # ***************************************************
-    pred = tx@w
-    loss = np.mean((y-pred)**2, axis=0)
-    return loss
-
-def compute_gradient(y, tx, w):
-    """Computes the gradient at w.
-    Args:
-        y: shape=(N, )
-        tx: shape=(N,2)
-        w: shape=(2, ). The vector of model parameters.
-    Returns:
-        An array of shape (2, ) (same shape as w), containing the gradient of the loss at w.
-    """
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: compute gradient vector
-    # ***************************************************
-    grad = 2*np.mean(tx.T*(tx@w-y), axis=1)
-
-    return grad 
-
 
 def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
     """The Gradient Descent (GD) algorithm.
@@ -81,37 +45,11 @@ def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
 
     return ws, losses
 
+
+
 """
 Stochastic Gradient Descent
 """
-
-
-def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
-    """
-    Generate a minibatch iterator for a dataset.
-    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
-    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
-    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
-    Example of use :
-    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
-        <DO-SOMETHING>
-    """
-    data_size = len(y)
-
-    if shuffle:
-        shuffle_indices = np.random.permutation(np.arange(data_size))
-        shuffled_y = y[shuffle_indices]
-        shuffled_tx = tx[shuffle_indices]
-    else:
-        shuffled_y = y
-        shuffled_tx = tx
-    for batch_num in range(num_batches):
-        start_index = batch_num * batch_size
-        end_index = min((batch_num + 1) * batch_size, data_size)
-        if start_index != end_index:
-            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
-    
-
 
 def mean_squared_error_sgd(y, tx, initial_w, batch_size, max_iters, gamma):
     """The Stochastic Gradient Descent algorithm (SGD).
@@ -140,88 +78,14 @@ def mean_squared_error_sgd(y, tx, initial_w, batch_size, max_iters, gamma):
         # ***************************************************
         for y_, tx_ in batch_iter(y, tx, batch_size=batch_size, num_batches=1):
             grad = compute_gradient(y_, tx_, w)
+            loss = compute_loss(y_, tx_, w)
             w = w - gamma*grad
-            loss = compute_loss(y, tx, w)
 
             ws.append(w)
             losses.append(loss)
 
     return ws, losses
 
-
-
-
-
-# linear regression using gradient descent
-def least_squares_GD(y, tx, initial_w=None, max_iters=100, gamma=0.25):
-    """
-    :param y: labels, size: (N,)
-    :param tx: features, size: (N,D)
-    :param initial_w: initial weight, size: (D,)
-    :param max_iters: number of steps to run
-    :param gamma: step size for (stochastic) gradient descent
-    :return: (w, loss): last weight vector and loss, size: ((D,), 1)
-    """
-    N = y.shape[0]
-    losses = []
-    w = initial_w if initial_w is not None else np.zeros(tx.shape[1])
-    for mini_y, mini_tx in batch_iter(y, tx, batch_size=N, num_batches=max_iters, shuffle=True):
-        loss = 0.5 * np.mean((mini_y - np.dot(mini_tx, w))**2)
-        e = mini_y - np.dot(mini_tx, w)
-        gradient = - np.dot(mini_tx.T, e) / N
-        w = w - (gamma*gradient)
-        losses.append(loss)
-    return w, losses[-1]
-
-
-# linear regression using stochastic gradient descent
-def least_squares_SGD(y, tx, initial_w=None, max_iters=450000, gamma=1e-3):
-    """
-    :param y: labels, size: (N,)
-    :param tx: features, size: (N,D)
-    :param initial_w: initial weight, size: (D,)
-    :param max_iters: number of steps to run
-    :param gamma: step size for (stochastic) gradient descent
-    :return: (w, loss): last weight vector and loss, size: ((D,), 1)
-    """
-    losses = []
-    w = initial_w if initial_w is not None else np.zeros(tx.shape[1])
-    for mini_y, mini_tx in batch_iter(y, tx, batch_size=1, num_batches=max_iters, shuffle=True):
-        loss = 0.5 * np.mean((mini_y - np.dot(mini_tx, w))**2)
-        e = mini_y - np.dot(mini_tx, w)
-        gradient = - np.dot(mini_tx.T, e)
-        w = w - (gamma*gradient)
-        losses.append(loss)
-    return w, losses[-1]
-
-
-# least squares regression using normal equations
-def least_squares(y, tx):
-    """
-    :param y: labels, size: (N,)
-    :param tx: features, size: (N,D)
-    :return: (w, loss): last weight vector and loss, size: ((D,), 1)
-    """
-    w = np.linalg.solve(np.dot(tx.T, tx), np.dot(tx.T, y))
-    loss = 0.5 * np.mean((y - np.dot(tx, w))**2)
-
-    return w, loss
-
-
-# ridge regression using normal equations
-def ridge_regression(y, tx, lambda_=1e-5):
-    """
-    :param y: labels, size: (N,)
-    :param tx: features, size: (N,D)
-    :param lambda_: regularization coefficient
-    :return: (w, loss): last weight vector and loss, size: ((D,), 1)
-    """
-    N = y.shape[0]
-    D = tx.shape[1]
-    w = np.linalg.solve(np.dot(tx.T, tx) + 2 * N *
-                        lambda_ * np.identity(D), np.dot(tx.T, y))
-    loss = 0.5 * np.mean((y - np.dot(tx, w))**2)
-    return w, loss
 
 
 def logistic_regression(y, tx, initial_w=None, max_iters=225000, gamma=2e-3):
