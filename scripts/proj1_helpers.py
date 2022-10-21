74b6c1ee9e6a1d401a1bfa8ce2a9ce884bd3bad7
eric11eca <chenz16@rose-hulman.edu>
2022-10-21 03:06:53 +0200

    add batch iter to helper

--- scripts/proj1_helpers.py
+++ scripts/proj1_helpers.py
@@ -46,3 +46,45 @@ def create_csv_submission(ids, y_pred, name):
         writer.writeheader()
         for r1, r2 in zip(ids, y_pred):
             writer.writerow({'Id':int(r1),'Prediction':int(r2)})
+
+
+# training data batch generator
+def batch_iter(y, tx, batch_size, num_batches=1, seed=7, shuffle=True):
+    """
+    :param y: labels, size: (N,)
+    :param tx: features, size: (N,D)
+    :param batch_size: size of mini_batch B
+    :param num_batches: number of batches generated K
+    :param seed: random seed for shuffle
+    :param shuffle: if shuffle the data
+    :return: mini-batch data: ((y_mini_batch_1, tx_mini_batch_1), ..., (y_mini_batch_K, tx_mini_batch_K))
+    """
+    data_size = y.shape[0]
+    np.random.seed(seed)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+
+    for batch_num in range(int(num_batches)):
+        start_index = int(batch_num * batch_size) % data_size
+        end_index = int((batch_num + 1) * batch_size) % data_size
+        if start_index < end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+        else:
+            if shuffle:
+                new_shuffle_indices = np.random.permutation(
+                    np.arange(data_size))
+                new_shuffled_y = y[new_shuffle_indices]
+                new_shuffled_tx = tx[new_shuffle_indices]
+            else:
+                new_shuffled_y = y
+                new_shuffled_tx = tx
+            yield np.r_[shuffled_y[start_index:], new_shuffled_y[:end_index]], np.r_[shuffled_tx[start_index:],
+                                                                                     new_shuffled_tx[:end_index]]
+            shuffled_y = new_shuffled_y
+            shuffled_tx = new_shuffled_tx
