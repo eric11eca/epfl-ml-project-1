b5ea97d016028171eb7f98e17c2ef24bfa4f97d9
Olivia-fsm <43227590+Olivia-fsm@users.noreply.github.com>
2022-10-21 15:30:44 +0200

    Update implementations.py

--- implementations.py
+++ implementations.py
@@ -124,9 +124,9 @@ def ridge_regression(y, tx, lambda_, return_loss=True):
     >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 1)
     array([0.03947092, 0.00319628])
     """
-    D = tx.shape[1]
+    N, D = tx.shape
 
-    inv = np.linalg.inv(tx.T.dot(tx) + lambda_ * np.identity(D))
+    inv = np.linalg.inv(tx.T.dot(tx) + 2*N*lambda_ * np.identity(D))
     w = inv.dot(tx.T).dot(y)
     if return_loss:
         loss = compute_loss(y, tx, w, opt='mse')
