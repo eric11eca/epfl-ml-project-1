d1e6cd393c0ec78b2ad8ca09489be2dcc11c4510
eric11eca <chenz16@rose-hulman.edu>
2022-10-22 13:59:44 +0200

    add new helpers and data preprocessor

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -1,23 +1,84 @@
 import numpy as np
 
-from proj1_helpers import load_csv_data
+from scripts.helpers import load_csv_data
 
 
 class Dataset:
-
     def __init__(self, data_pth, data_type):
-        self.data_pth = f"{self.data_pth}/{self.data_type}.csv"
+        self.data_pth = f"{data_pth}/{data_type}.csv"
         self.data_type = data_type
 
         self.data = []
+        self.labels = []
+        self.ids = []
+
+        self.col_names = self.read_col_names()
+        self.num_cols = len(self.col_names)
 
     def load_data(self):
+        """Load the data from the csv file."""
+
         y, tX, ids = load_csv_data(self.data_pth)
         self.ids = ids
         self.labels = y
         self.data = tX
 
+        self.data_imputation()
+        self.data_normalization()
+        self.filter_outliers()
+
     def read_col_names(self):
+        """Read the column names from the csv file."""
+
         with open(self.data_pth, "r") as f:
             col_names = f.readline().strip().split(",")
         return col_names
+
+    def data_imputation(self, method="median"):
+        """Impute the missing values in the data."""
+
+        for col in range(self.data.shape[1]):
+            col_data = self.data[:, col]
+            if method == "mean":
+                col_data[col_data == -
+                         999.0] = np.nanmean(col_data[col_data != -999.0])
+                col_data[np.isnan(col_data)] = np.nanmean(col_data)
+            elif method == "median":
+                col_data[col_data == -
+                         999.0] = np.nanmedian(col_data[col_data != -999.0])
+                col_data[np.isnan(col_data)] = np.nanmedian(col_data)
+            else:
+                col_data[col_data == -999.0] = 0.0
+                col_data[np.isnan(col_data)] = 0.0
+
+    def data_normalization(self):
+        """Normalize the data, zero-mean and standardization."""
+
+        mean_data = np.mean(self.data, axis=0)
+        self.data = self.data - mean_data
+        self.data = self.data / np.std(self.data, axis=0)
+
+    def category_feature(self):
+        """Create new features based on the category feature."""
+        jet_num_one_hot = {
+            "0": [0.0, 0.0, 0.0, 1.0],
+            "1": [0.0, 0.0, 1.0, 0.0],
+            "2": [0.0, 1.0, 0.0, 0.0],
+            "3": [1.0, 0.0, 0.0, 0.0]
+        }
+
+        jet_num_col = self.col_names.index("PRI_jet_num")
+
+    def filter_outliers(self, m=10):
+        """
+        Filter out outliers over mean +/- m * std>
+        """
+        for i in range(self.data.shape[1]):
+            delta = abs(self.data[:, i] - np.mean(self.data[:, i]))
+            mdev = m * np.std(self.data[:, i])
+            self.data = self.data[delta < mdev]
+            self.labels = self.labels[delta < mdev]
+            self.ids = self.ids[delta < mdev]
+
+            assert self.labels.shape[0] == self.data.shape[0]
+            assert self.ids.shape[0] == self.data.shape[0]
--- scripts/helpers.py
+++ scripts/helpers.py
@@ -3,6 +3,8 @@
 import csv
 import numpy as np
 
+from itertools import product
+
 
 def load_csv_data(data_path, sub_sample=False):
     """Loads data and returns y (class labels), tX (features) and ids (event ids)"""
@@ -74,3 +76,60 @@ def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
         end_index = min((batch_num + 1) * batch_size, data_size)
         if start_index != end_index:
             yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
+
+def build_parameter_grid(param_options):
+    for item in product(*param_options.values()):
+        yield dict(zip(param_options.keys(), item))
+
+
+def predict_binary(y, tx, w, loss_type="logistic"):
+    """
+    :param y: ground truth labels, size: (N_Test,)
+    :param tx: input features, size: (N_Test,D)
+    :param w: trained weights
+    :param loss_type: model type: ["mse", "rmse", "logistic"]
+    :return: predict_y, test_loss
+    """
+    z = np.dot(tx, w)
+    if loss_type == "mse":
+        test_loss = 0.5 * np.mean((y - z)**2)
+    elif loss_type == "rmse":
+        test_loss = np.sqrt(np.mean((y - z)**2))
+    elif loss_type == "logistic":
+        test_loss = np.mean(np.log(1 + np.exp(z)) - y * z)
+    else:
+        raise ValueError("loss_type must be mse, rmse or logistic")
+
+    if loss_type == "logistic":
+        p_pred = np.exp(z) / (1 + np.exp(z))
+    else:
+        p_pred = z
+
+    predict_y = list(map(lambda x: 0 if x < 0.5 else 1, p_pred))
+
+    return predict_y, test_loss
+
+def compute_prf_binary(label_y, predict_y):
+    """
+    :param label_y: ground truth labels
+    :param predict_y: model predictions
+    :return: precision, recall, f1
+    """
+    tp, fp, tn, fn = 1e-5, 1e-5, 1e-5, 1e-5
+    for idx, label in enumerate(label_y):
+        if label == 1:
+            if predict_y[idx] == 1:
+                tp += 1
+            else:
+                fn += 1
+        else:
+            if predict_y[idx] == 1:
+                fp += 1
+            else:
+                tn += 1
+    accuracy = (tp + tn) / (tp + fp + tn + fn)
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f1 = 2 * precision * recall / (precision + recall)
+    return accuracy, precision, recall, f1
